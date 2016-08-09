e2484e712601e581db1d43d1dcafd365061ca481
Atul Kumar <atulbk@thoughtworks.com>
2016-08-09 19:19:02 +0530

    Replaced List with Map

--- TWU_Biblioteca-master/src/com/twu/biblioteca/Library.java
+++ TWU_Biblioteca-master/src/com/twu/biblioteca/Library.java
@@ -1,24 +1,26 @@
 package com.twu.biblioteca;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 // Represents the collections of book
 public class Library {
 
-	List<Book> checkoutBooks= new ArrayList<Book>();
-	List<Book> availableBooks = new ArrayList<Book>();
-	public String getWelcomeMessage(){
+	Map<Integer, Book> availableBooks = new HashMap<Integer, Book>();
+	Map<Integer, Book> checkoutBooks = new HashMap<Integer, Book>();
+
+	public String getWelcomeMessage() {
 		return "Welcome";
 	}
 
-	public void addBooks(){
-		Book book1=new Book(1,"Java","Malik",2010);
-		Book book2=new Book(2,"Nancy Drew","Carolyn Keene",2008);
-		Book book3=new Book(3,"Artemis Fowl","Eoin Colfer",2005);
-		availableBooks.add(book1);
-		availableBooks.add(book2);
-		availableBooks.add(book3);
+	public void addBooks() {
+
+
+		Book book1 = new Book(1, "Java", "Malik", 2010);
+		Book book2 = new Book(2, "Nancy Drew", "Carolyn Keene", 2008);
+		Book book3 = new Book(3, "Artemis Fowl", "Eoin Colfer", 2005);
+		availableBooks.put(1, book1);
+		availableBooks.put(2, book2);
+		availableBooks.put(3, book3);
 	}
 
 	public Library() {
@@ -29,42 +31,30 @@ public class Library {
 
 	public List<String> printBookList() {
 
-		List<String> bookDetails=new ArrayList<String>();
-		for (Book book : availableBooks) {
+		List<String> bookDetails = new ArrayList<String>();
+		for (Book book : availableBooks.values()) {
 			bookDetails.add(book.printBookDetails());
 		}
 		return bookDetails;
 	}
 
-	public String  checkout(int ISBN) {
-		boolean bookAvailable=false;
-	for(Book book:availableBooks){
-		if(book.contains(ISBN))
-			{
-				 bookAvailable = true;
-				swapBooks(book);
-			}
+	public String checkout(int ISBN) {
+		if (availableBooks.containsKey(ISBN)) {
+			Book book = availableBooks.remove(ISBN);
+			checkoutBooks.put(ISBN, book);
+			return "Thank you! Enjoy the book";
 		}
-		if(bookAvailable)
-		return "Thank you! Enjoy the book";
-	return "That book is not available";
-	}
-
-	private void swapBooks(Book book) {
-		availableBooks.remove(book);
-		checkoutBooks.add(book);
+		return "That book is not available";
 	}
 
 	public void returnBook(int ISBN) {
-		for(Book book:checkoutBooks){
-			if(book.contains(ISBN)) {
-				swapBooksForBooks(book);
-			}
+		if (checkoutBooks.containsKey(ISBN)) {
+			Book book = checkoutBooks.remove(ISBN);
+			availableBooks.put(ISBN, book);
 		}
+
+
 	}
 
-	private void swapBooksForBooks(Book book) {
-	checkoutBooks.remove(book);
-		availableBooks.add(book);
-	}
+
 }
