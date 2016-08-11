01f90ca2d28405edd80b2e96fd4c6496f5ce037b
Atul Kumar <atulbk@thoughtworks.com>
2016-08-11 18:37:45 +0530

    Implemented Common print function For Book and Movie

--- TWU_Biblioteca-master/src/IO/Reader.java
+++ TWU_Biblioteca-master/src/IO/Reader.java
@@ -1,13 +1,39 @@
 package IO;
 
-
 import com.twu.biblioteca.Book;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Map;
+import java.util.*;
 
-public interface Reader {
+// Takes Input
+public class Input implements ConsoleReader {
 
-  int receiveInput();
-	Map<Integer,Book> fetchFromFile() throws IOException;
+	public int receiveInput() {
+		int option = 0;
+		Scanner input = new Scanner(System.in);
+		try {
+			option = input.nextInt();
+		} catch (InputMismatchException e) {
+		}
+		return option;
+	}
+
+	public Map<Integer,Book> fetchFromFile(int choice) throws IOException {
+		String book;
+		List<String> listOfBooks = new ArrayList<String>();
+		FileReader reader;
+		if(choice==1){
+		reader = new FileReader("/Users/atulk/Desktop/Assignment/booklist.txt");}
+		else {
+			reader = new FileReader("/Users/atulk/Desktop/Assignment/movielist.txt");}
+
+
+		BufferedReader buffer = new BufferedReader(reader);
+		while ((book = buffer.readLine()) != null) {
+			listOfBooks.add(book);
+		}
+			return (new parseInput().createMapOfBooks(listOfBooks));
+	}
 }
---
+++ TWU_Biblioteca-master/test/com/twu/biblioteca/LibraryItemTest.java
@@ -0,0 +1,7 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by atulk on 8/11/16.
+ */
+public class LibraryItemTest {
+}
